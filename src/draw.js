d0f73536b4ce62ed2a513143c7279c084f540e36
Pedro Gardim <me@pedrogardim.com>
2023-11-30 09:34:47 +0100

    feat: interface

--- src/draw.js
+++ src/draw.js
@@ -1,3 +1,4 @@
+import readline from "readline";
 import colors from "@colors/colors";
 import { appState } from "./main.js";
 import { select, input } from "@inquirer/prompts";
@@ -5,12 +6,26 @@ import { initSongProcess } from "./main.js";
 
 //TEMP
 import events from "events";
-
 events.EventEmitter.defaultMaxListeners = 9999;
+
+const choices = [
+  {
+    name: "Generate a video",
+    value: "add",
+  },
+  {
+    name: "End program",
+    value: "end",
+  },
+];
+
+let selectedOption = 0;
+let isInsertingUrl = false;
 
 export const draw = async () => {
   console.clear();
   process.stdout.write("\x1Bc");
+  // inputInterface.close();
   console.log("==MUSICVID AUTOMATOR 📹🎶==\n\n");
 
   if (!Object.values(appState.songs).length) {
@@ -64,33 +79,90 @@ export const draw = async () => {
 
   appState.errorMessage && console.log(appState.errorMessage.red + "\n");
 
-  const answer = await select(
-    {
-      message: "What should we do?",
-      choices: [
-        {
-          name: "Generate a video",
-          value: "add",
-        },
-        {
-          name: "End program",
-          value: "end",
-        },
-      ],
-    },
-    { clearPromptOnDone: true }
-  );
+  console.log("What should we do?");
+
+  if (isInsertingUrl) {
+    inputInterface.question("Enter YouTube URL:", (url) => {
+      initSongProcess(url);
+      inputInterface.close();
+    });
+  } else {
+    choices.forEach((choice, index) => {
+      const choicePrint =
+        index === selectedOption ? `› ${choice.name}`.cyan : `  ${choice.name}`;
+      console.log(choicePrint);
+    });
+  }
+
+  process.stdin.resume();
+  return;
+
+  const answer = await select({
+    message: "What should we do?",
+    choices: [
+      {
+        name: "Generate a video",
+        value: "add",
+      },
+      {
+        name: "End program",
+        value: "end",
+      },
+    ],
+  });
 
   if (answer === "end") {
     console.clear();
     process.exit();
   }
   if (answer === "add") {
+    inputInterface.question("Enter YouTube URL:", (url) => {
+      initSongProcess(url);
+      inputInterface.close();
+    });
+
     const url = await input(
       { message: "Enter YouTube URL:" },
       { clearPromptOnDone: true }
     );
-    initSongProcess(url);
   }
   draw();
 };
+
+readline.emitKeypressEvents(process.stdin);
+
+export const inputInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+process.stdin.on("keypress", (ch, key) => {
+  switch (key?.name) {
+    case "up":
+      selectedOption++;
+      selectedOption %= 2;
+      selectedOption = Math.abs(selectedOption);
+      draw();
+      break;
+    case "down":
+      selectedOption--;
+      selectedOption %= 2;
+      selectedOption = Math.abs(selectedOption);
+      draw();
+      break;
+    case "c":
+      if (key?.ctrl) {
+        console.clear();
+        process.exit();
+      }
+      break;
+    case "return":
+      isInsertingUrl = true;
+      draw();
+      break;
+    default:
+      break;
+  }
+});
+
+process.stdin.setRawMode(true);
